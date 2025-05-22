93829017cf872d1ef0aa3174420f05b5d2c4fd24
Janne <tapioninetytwo@live.se>
2025-05-22 20:33:35 +0200

    Added folders to begin building the auth system

---
+++ Presentation/Models/RepoResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Presentation.Models;
+
+public class RepoResult
+{
+    public bool Success { get; set; }
+    public string? Error { get; set; }
+
+}
+public class RepoResult<T> : RepoResult
+{
+    public T? Data { get; set; }
+}
